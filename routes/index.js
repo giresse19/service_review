e045daeb603f4aac05d78081d727008bae8baa94
GIRESSE ASHU-BOB <ashubobg@yahoo.co.uk>
2017-05-28 14:54:54 +0300

    adding all files

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/store');
+
+router.get('/', storeController.homePage);
+
+module.exports = router;
