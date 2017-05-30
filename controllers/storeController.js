56954496cee8f12b8bd7855ca17ce2618d329d4e
GIRESSE ASHU-BOB <ashubobg@yahoo.co.uk>
2017-05-30 20:02:43 +0300

    rearranging edit

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -21,3 +21,25 @@ exports.getStores = async (req, res) => {
 	res.render('stores', {title: 'Stores', stores: stores }); 
 };
 
+exports.editStore = async (req, res) => {
+	// find a store base on the id
+	const store = await Store.findOne({ _id: req.params.id })
+	// res.json(store);
+	//  TODO confirm they are the owner of the store
+
+	// render out the edit form, so user can update
+	res.render('editStore', { title: `Edit ${store.name}`, store: store });
+};
+
+exports.updateStore = async (req, res) => {
+	// find and update the store
+	const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+	  new: true, // return new store instead of old one	  runValidators: true
+	}).exec(); // we use exec just o ensure the new updated data is the one coming
+	
+	req.flash('success', `successfully Updated <strong>${store.name}</strong>, 
+		<a href="/stores/${store.slug}"> View Store</a>`);
+	res.redirect(`/stores/${store._id}/edit`);
+	// redirect then the store, and tell them it worked
+};
+
